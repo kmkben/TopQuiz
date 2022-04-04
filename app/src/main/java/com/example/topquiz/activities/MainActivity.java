79819a27a8bb84229e04052af706a7bb6c45b940
Kemoko KEITA <kemokokeita710@gmail.com>
2022-04-04 23:55:48 +0100

    Save the name of User and his Score

--- app/src/main/java/com/example/topquiz/activities/MainActivity.java
+++ app/src/main/java/com/example/topquiz/activities/MainActivity.java
@@ -19,6 +19,7 @@ import com.example.topquiz.model.User;
 public class MainActivity extends AppCompatActivity {
 
     private TextView nameText;
+    private TextView welcomeText;
     private EditText name;
     private Button play;
 
@@ -26,16 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int GAME_ACTIVITY_REQUEST_CODE = 7;
 
+    private static final String SHARED_PREF_USER_INFO = "SHARED_PREF_USER_INFO";
+    private static final String SHARED_PREF_USER_INFO_NAME = "SHARED_PREF_USER_INFO_NAME";
+    private static final String SHARED_PREF_USER_INFO_SCORE = "SHARED_PREF_USER_INFO_SCORE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        welcomeText = (TextView) findViewById(R.id.welcomeText);
         nameText = (TextView) findViewById(R.id.nameEditTex);
         name = (EditText) findViewById(R.id.nameEditText);
         play = (Button) findViewById(R.id.btnPlay);
 
         play.setEnabled(false);
+
+        user = new User();
+
+
 
         name.addTextChangedListener(new TextWatcher() {
             @Override
@@ -51,18 +61,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
                 play.setEnabled(!editable.toString().isEmpty());
+
             }
         });
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //user.setName(name.getText().toString());
+                user.setName(name.getText().toString());
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                        .edit()
+                        .putString(SHARED_PREF_USER_INFO_NAME, user.getName())
+                        .apply();
+
+
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 //startActivity(intent);
                 startActivityForResult(intent, GAME_ACTIVITY_REQUEST_CODE);
             }
         });
+
+        //greetUser();
     }
 
     @Override
@@ -71,6 +90,31 @@ public class MainActivity extends AppCompatActivity {
 
         if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
             int score = data.getIntExtra(GameActivity.RESULT_SCORE, 0);
+
+            getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                    .edit()
+                    .putInt(SHARED_PREF_USER_INFO_SCORE, score)
+                    .apply();
+
+            //greetUser();
+        }
+    }
+
+    private void greetUser() {
+        String userName = getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getString(SHARED_PREF_USER_INFO_NAME, null);
+        int score = getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getInt(SHARED_PREF_USER_INFO_SCORE, -1);
+
+        if (userName != null) {
+
+            if (score != -1) {
+                welcomeText.setText(getString(R.string.hello_user, userName));
+                nameText.setText(getString(R.string.welcome_back_with_score, userName, score));
+            } else {
+                welcomeText.setText(getString(R.string.hello_user, userName));
+                nameText.setText(getString(R.string.welcome_back, userName));
+            }
+
+            name.setText(userName);
         }
     }
 }
